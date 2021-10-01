0c65efb24769945303e21cc64c3d56f63d39b5ad
valis78 <91526764+valis78@users.noreply.github.com>
2021-10-01 10:59:19 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.right')
+
+burger.addEventListener('click', () => {
+
+    menu.classList.toggle('active')
+
+})
